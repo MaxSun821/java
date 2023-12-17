28f4d2766546757b7e08eb768cc0a0151a1cc75e
maxsun821 <maxsun8216@gmail.com>
2023-12-17 17:21:49 +0800

    员工类，日期类

---
+++ JavaSE/JavaSECode/chapter06/src/com/max/test2/MyDate.java
@@ -0,0 +1,58 @@
+package com.max.test2;
+
+/**
+ * ClassName: MyDate
+ * Package: com.max.test2
+ * Description: 日期类
+ *
+ * @Author MaxSun
+ * @Create 2023/12/17 16:58
+ * @Version 1.0
+ */
+public class MyDate {
+    private int year;
+    private int month;
+    private int day;
+
+    public MyDate() {
+    }
+
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    @Override
+    public String toString() {
+        return "MyDate{" +
+                "year=" + year +
+                ", month=" + month +
+                ", day=" + day +
+                '}';
+    }
+}
