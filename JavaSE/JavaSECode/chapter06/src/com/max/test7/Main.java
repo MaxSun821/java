39967dd922192e8d2e411d8cae35994f0d4e3279
maxsun821 <maxsun8216@gmail.com>
2023-12-18 13:15:21 +0800

    方法重载

---
+++ JavaSE/JavaSECode/chapter06/src/com/max/test7/Main.java
@@ -0,0 +1,19 @@
+package com.max.test7;
+
+/**
+ * ClassName: Main
+ * Package: com.max.test7
+ * Description: 测试类
+ *
+ * @Author MaxSun
+ * @Create 2023/12/18 13:13
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        Exer1 e1 = new Exer1();
+        e1.mOL(10);
+        e1.mOL(6, 9);
+        e1.mOL("我超强");
+    }
+}
