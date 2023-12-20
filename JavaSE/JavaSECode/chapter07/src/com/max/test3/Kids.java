351f4248f2feaba155824a89bb84eb8adb1c9cd8
maxsun821 <maxsun8216@gmail.com>
2023-12-20 13:07:20 +0800

    方法重写

---
+++ JavaSE/JavaSECode/chapter07/src/com/max/test3/Kids.java
@@ -0,0 +1,31 @@
+package com.max.test3;
+
+/**
+ * ClassName: Kids
+ * Package: com.max.test1
+ * Description:
+ *
+ * @Author MaxSun
+ * @Create 2023/12/20 12:45
+ * @Version 1.0
+ */
+public class Kids extends ManKind {
+    private int yearsOld;
+
+    public Kids() {
+    }
+
+    public int getYearsOld() {
+        return yearsOld;
+    }
+
+    public void setYearsOld(int yearsOld) {
+        this.yearsOld = yearsOld;
+    }
+    public void employeed() {
+        System.out.println("Kids should study and no job");
+    }
+    public void printAge() {
+        System.out.println("年龄：" + yearsOld);
+    }
+}
