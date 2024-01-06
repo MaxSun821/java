cacbc811ba257e03e66c5b721dd2d517634571bb
maxsun821 <maxsun8216@gmail.com>
2024-01-06 15:06:22 +0800

    Set集合测试

--- JavaSE/JavaSECode/chapter12/src/com/max/test3/SetTest.java
+++ JavaSE/JavaSECode/chapter12/src/com/max/test3/SetTest.java
@@ -1,14 +1,12 @@
 package com.max.test3;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * ClassName: SetTest
  * Package: com.max.test3
  * Description: 去掉List中的重复元素
+ * 获取10个1至20的随机数，要求随机数不能重复。并把最终的随机数输出到控制台。
  *
  * @Author MaxSun
  * @Create 2024/1/6 14:53
@@ -24,10 +22,17 @@ public class SetTest {
         list.add(5);
 
         List<Integer> list1 = duplicateList(list);
+
+        Random r = new Random();
+        Set<Integer> set = new HashSet<>();
+        while(set.size() <= 10) {
+            int tmp = r.nextInt(20) + 1;
+            set.add(tmp);
+        }
+        System.out.println(set.toString());
     }
     public static List<Integer> duplicateList(List<Integer> list) {
         Set<Integer> set = new HashSet<>(list);
-        System.out.println(set.toString());
         List<Integer> list1 = new ArrayList<>(set);
         return list1;
     }
