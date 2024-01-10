58ef71afe90b8df3c67a4131a0413c71a10d5766
maxsun821 <maxsun8216@gmail.com>
2024-01-10 20:21:08 +0800

    删除

--- JavaSE/chapter01/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Name: Max");
-        System.out.println("Gender: FEMALE");
-        System.out.println("Address: China");
-    }
-}
