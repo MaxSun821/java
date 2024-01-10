e6824a7ece11944bc29cea247a200b1337c79b5b
maxsun821 <maxsun8216@gmail.com>
2024-01-10 20:31:54 +0800

    Hello World

---
+++ JavaSE/Variable/src/com/max/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.max.helloworld;
+
+/**
+ * ClassName: HelloWorld
+ * Package: com.max.helloworld
+ * Description: Hello World
+ *
+ * @Author MaxSun
+ * @Create 2024/1/10 20:31
+ * @Version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
