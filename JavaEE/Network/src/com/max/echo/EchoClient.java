f933caa606ccd0bacad323b2aac46e263fc5fdc3
maxsun821 <maxsun8216@gmail.com>
2024-02-25 18:15:18 +0800

    udp网络编程

---
+++ JavaEE/Network/src/com/max/echo/EchoClient.java
@@ -0,0 +1,53 @@
+package com.max.echo;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+/**
+ * ClassName: EchoClient
+ * Package: com.max.echo
+ * Description: 回显通讯客户端
+ *
+ * @Author MaxSun
+ * @Create 2024/2/25 17:03
+ * @Version 1.0
+ */
+public class EchoClient {
+    private DatagramSocket datagramSocket;
+    private String serverAddr;
+    private int serverPort;
+
+    public EchoClient(String serverAddr, int serverPort) throws SocketException {
+        datagramSocket = new DatagramSocket(); // 客户端不需要指定端口号，只需要系统自动分配即可
+        this.serverAddr = serverAddr;
+        this.serverPort = serverPort;
+    }
+
+    public void start() throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            // 客户端输入请求
+            System.out.print("-> ");
+            String request = sc.next();
+            // 客户端发送请求
+            DatagramPacket requestPacket = new DatagramPacket(request.getBytes(), request.getBytes().length, InetAddress.getByName(serverAddr), serverPort);
+            datagramSocket.send(requestPacket);
+
+            // 客户端接收服务器响应
+            DatagramPacket responsePacket = new DatagramPacket(new byte[4096], 4096);
+            datagramSocket.receive(responsePacket);
+
+            String response = new String(responsePacket.getData(), 0, responsePacket.getLength());
+            // 打印
+            System.out.printf("req: %s resp: %s\n", request, response);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        EchoClient echoClient = new EchoClient("127.0.0.1", 9090);
+        echoClient.start();
+    }
+}
---
+++ JavaEE/Network/src/com/max/echo/EchoServer.java
@@ -0,0 +1,58 @@
+package com.max.echo;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+
+/**
+ * ClassName: EchoServer
+ * Package: com.max.echo
+ * Description: 回显通讯服务器
+ *
+ * @Author MaxSun
+ * @Create 2024/2/25 17:02
+ * @Version 1.0
+ */
+public class EchoServer {
+    private DatagramSocket datagramSocket;
+
+    public EchoServer(int port) throws SocketException {
+        datagramSocket = new DatagramSocket(port); // 服务器需要指定端口号
+    }
+
+
+    /**
+     * 启动服务器方法
+     */
+    public void start() throws IOException {
+        System.out.println("服务器启动");
+        while (true) {
+            // 服务器从客户端接收数据
+            DatagramPacket requestPacket = new DatagramPacket(new byte[4096], 4096); // 接收客户端发来的请求存入的DatagramPacket对象
+            datagramSocket.receive(requestPacket);
+
+            // 服务器解析请求数据
+            String request = new String(requestPacket.getData(), 0, requestPacket.getLength());
+            String response = process(request);
+
+            // 服务器返回响应给客户端
+            DatagramPacket responsePacket = new DatagramPacket(response.getBytes(), response.getBytes().length, requestPacket.getSocketAddress());
+            datagramSocket.send(responsePacket);
+
+            // 打印日志
+            System.out.printf("[%s:%d] req: %s resp: %s\n", requestPacket.getAddress().toString(), requestPacket.getPort(), request, response);
+        }
+    }
+
+    public String process(String request) {
+        return request;
+    }
+
+    public static void main(String[] args) throws IOException {
+        EchoServer echoServer = new EchoServer(9090);
+
+        echoServer.start();
+    }
+}
